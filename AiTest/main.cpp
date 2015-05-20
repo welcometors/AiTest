cb0642191b47fc716b2821d2e987eb4cad9ceb97
welcometors <welcometors@gmail.com>
2015-05-20 22:20:47 +0530

    xor done

--- AiTest/main.cpp
+++ AiTest/main.cpp
@@ -3,8 +3,8 @@
 #include <cmath>
 #include "SimpleNeuralNetwork.h"
 
-const int numOfTrainningSamples = 2000;
-const int numOfTestSamples = 10;
+const int numOfTrainningSamples = 1000;
+const int numOfTestSamples = numOfTrainningSamples * .15;
 
 int main(){
 	SimpleNeuralNetwork snn({ 2, 4, 1 });
@@ -22,6 +22,25 @@ int main(){
 
 		printf("Training: %d ^ %d = %d, out: %f, RA: %f\n", a, b, c, outputVals[0], snn.getRunningAverage());
 	}
+	printf("Training done, running average: %f\n", snn.getRunningAverage());
 
+	int correct = 0;
+	for (int i = 0; i < numOfTestSamples; i++){
+		int a = rand() & 1, b = rand() & 1, c = a^b;
+		inputVals[0] = a;
+		inputVals[1] = b;
+		
+		snn.feedForward(inputVals);
+		snn.getResults(outputVals);
+		int d = int(outputVals[0] + 0.5);
+		
+		printf("Testing: %d ^ %d = %d, out: %d\n", a, b, c, d);
+		if (c == d){
+			correct++;
+		}
+	}
+
+	printf("%d out of %d (%3.2f%%) succeeded after traing %d sample data.\n",
+		correct, numOfTestSamples, 100.0*correct/numOfTestSamples, numOfTrainningSamples);
 	system("pause");
 } 
