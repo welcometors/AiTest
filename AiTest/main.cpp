b634d63c09fb3624af706ae34aa8bc3a0015924c
welcometors <welcometors@gmail.com>
2015-05-21 19:52:20 +0530

    training data

--- AiTest/main.cpp
+++ AiTest/main.cpp
@@ -3,44 +3,62 @@
 #include <cmath>
 #include "SimpleNeuralNetwork.h"
 
-const int numOfTrainningSamples = 1000;
+#define DEBUG
+const int numOfTrainningSamples = 10000;
 const int numOfTestSamples = numOfTrainningSamples * .15;
+
+template<size_t I, size_t O>
+struct TraningData{
+	std::vector<double> inputVals;
+	std::vector<double> outputVals;
+	std::vector<double> actualVals;
+
+	TraningData(){
+		double ang = (1 + (rand() % 78)) / 100.0;
+		//if (rand() & 1) ang *= -1;
+		inputVals.push_back(cos(ang));
+		inputVals.push_back(sin(ang));
+		actualVals.push_back(ang);
+		outputVals.push_back(0);
+	}
+
+	void print(double ra){
+#ifdef DEBUG
+		//printf("Training: %d ^ %d = %d, out: %f, RA: %f\n", a, b, c, outputVals[0], snn.getRunningAverage());
+		printf("Training: tan-1(%f, %f) = %f, out: %f, RA: %f\n", inputVals[0], inputVals[1], actualVals[0], outputVals[0], ra);
+#endif
+	}
+};
 
 int main(){
 	SimpleNeuralNetwork snn({ 2, 4, 1 });
-
-	std::vector<double> inputVals(2), outputVals, actualVals(1);
+	
 	for (int i = 0; i < numOfTrainningSamples; i++){
-		int a = rand() & 1, b = rand() & 1, c = a^b;
-		inputVals[0] = a;
-		inputVals[1] = b;
-		actualVals[0] = c;
-
-		snn.feedForward(inputVals);
-		snn.getResults(outputVals);
-		snn.backPropogation(actualVals);
-
-		printf("Training: %d ^ %d = %d, out: %f, RA: %f\n", a, b, c, outputVals[0], snn.getRunningAverage());
+		TraningData<2, 1> trainingData;
+		snn.feedForward(trainingData.inputVals);
+		snn.getResults(trainingData.outputVals);
+		snn.backPropogation(trainingData.actualVals);
+		//trainingData.print(snn.getRunningAverage());
 	}
 	printf("Training done, running average: %f\n", snn.getRunningAverage());
 
-	int correct = 0;
+	double rms = 0.0;
 	for (int i = 0; i < numOfTestSamples; i++){
-		int a = rand() & 1, b = rand() & 1, c = a^b;
-		inputVals[0] = a;
-		inputVals[1] = b;
-		
-		snn.feedForward(inputVals);
-		snn.getResults(outputVals);
-		int d = int(outputVals[0] + 0.5);
-		
-		printf("Testing: %d ^ %d = %d, out: %d\n", a, b, c, d);
-		if (c == d){
-			correct++;
-		}
+		TraningData<2, 1> trainingData;
+		snn.feedForward(trainingData.inputVals);
+		snn.getResults(trainingData.outputVals);
+		trainingData.print(snn.getRunningAverage());
+		rms += (trainingData.actualVals[0] - trainingData.outputVals[0])*(trainingData.actualVals[0] - trainingData.outputVals[0]);
 	}
 
-	printf("%d out of %d (%3.2f%%) succeeded after traing %d sample data.\n",
-		correct, numOfTestSamples, 100.0*correct/numOfTestSamples, numOfTrainningSamples);
+	TraningData<2, 1> trainingData;
+	trainingData.inputVals[0] = 0.93937271284737892003503235730367;
+	trainingData.inputVals[1] = 0.34289780745545134918963490691763;
+	trainingData.actualVals[0] = 0.35;
+	snn.feedForward(trainingData.inputVals);
+	snn.getResults(trainingData.outputVals);
+	trainingData.print(snn.getRunningAverage());
+
+	printf("Average error is (%3.5f%%) after traing %d sample data.\n", sqrt(rms / numOfTestSamples), numOfTrainningSamples);
 	system("pause");
 } 
